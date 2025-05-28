19942394a070e8408bd7db42ea4c38bbe5bacd7c
Mohammad Shohel (Eshan) <mohammadshohel866@gmail.com>
2025-05-28 18:36:47 +0600

    Add API resource for Client response formatting

---
+++ app/Http/Resources/ClientResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'contact_person' => $this->contact_person,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'projects' => ProjectResource::collection($this->whenLoaded('projects')),
+        ];
+    }
+}
