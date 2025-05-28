061b7b655cf627d4d12677359dd994baef6e6b6d
Mohammad Shohel (Eshan) <mohammadshohel866@gmail.com>
2025-05-28 21:30:42 +0600

    Add repository and service layer for TimeLog

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password123'),
+            'email_verified_at' => now(),
+            'remember_token' => null,
+        ]);
+    }
+}
