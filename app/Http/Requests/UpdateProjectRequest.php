846e2c7fa05bf3425424f3ac7123caf5abb481c5
Mohammad Shohel (Eshan) <mohammadshohel866@gmail.com>
2025-05-28 20:19:25 +0600

    Add form request validation for Project

---
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|required|string|max:255',
+            'description' => 'sometimes|nullable|string',
+            'client_id' => 'sometimes|required|exists:clients,id',
+            'status' => 'required|in:active,completed',
+            'deadline' => 'sometimes|nullable|date',
+        ];
+    }
+}
